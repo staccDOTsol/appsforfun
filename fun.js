331c562ff2345592e875863adb8276c160a4ecf8
Jarett Dunn <jarettrsdunn+git@gmail.com>
2022-11-19 08:43:32 +0000

    ah

--- fun.js
+++ fun.js
@@ -235,8 +235,8 @@ else {
     prompt = "this is a chatbot that rephrases the original input to sound much more genuinely informed about " + topic + " keeping the theme, tone, and intention of the original input intact, while never including adult or risquee content.\n\n"
 }
 let tprompts = []
-
-for (var p of theprompts[uuid]){
+let urr = uuid
+for (var p of theprompts[urr]){
  p = p.replace(uuid, Math.random().toString())
   tprompts.push(p)
 }
