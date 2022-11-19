b7115300301226fa25ccf9f119c6366cbdbb0768
Jarett Dunn <jarettdunn+git@gmail.com>
2022-11-19 17:59:59 -0400

    migration

--- fun.js
+++ fun.js
@@ -114,24 +114,33 @@ try {
 let uuid = req.query.uuid
 
 let topic = req.query.topic 
-let prompt =  (await getConversation(db, topic, uuid)).join('\n') + "\n"+req.query.question
+let prompt =  (await getConversation(db, topic, uuid)).join('\n') + "\nFrancine: "+req.query.question+'!!!\n'+"Carlos: "
 
 const answer = await openai.createCompletion({
     model: "davinci:ft-personal-2022-11-19-20-53-39",
-    prompt,
+    prompt: "This is a dialogue horror story written by Stephen King. Stephen Edwin King (born September 21, 1947) is an American author of horror, supernatural fiction, suspense, crime, science-fiction, and fantasy novels. Described as the \"King of Horror\", a play on his surname and a reference to his high standing in pop culture,[2] his books have sold more than 350 million copies,[3] and many have been adapted into films, television series, miniseries, and comic books. King has published 64 novels, including seven under the pen name Richard Bachman, and five non-fiction books.[4] He has also written approximately 200 short stories, most of which have been published in book collections.[5][6] King has received Bram Stoker Awards, World Fantasy Awards, and British Fantasy Society Awards. In 2003, the National Book Foundation awarded him the Medal for Distinguished Contribution to American Letters.[7] He has also received awards for his contribution to literature for his entire bibliography, such as the 2004 World Fantasy Award for Life Achievement and the 2007 Grand Master Award from the Mystery Writers of America.[8] In 2015, he was awarded with a National Medal of Arts from the U.S. National Endowment for the Arts for his contributions to literature.[9]King's formula for learning to write well is: 'Read and write four to six hours a day. If you cannot find the time for that, you can't expect to become a good writer.' He sets out each day with a quota of 2000 words and will not stop writing until it is met. He also has a simple definition for talent in writing: 'If you wrote something for which someone sent you a check, if you cashed the check and it didn't bounce, and if you then paid the light bill with the money, I consider you talented.'[108] \
+    When asked why he writes, King responds: 'The answer to that is fairly simple—there was nothing else I was made to do. I was made to write stories and I love to write stories. That's why I do it. I really can't imagine doing anything else and I can't imagine not doing what I do.'[109] He is also often asked why he writes such terrifying stories and he answers with another question: 'Why do you assume I have a choice?'[110] King usually begins the story creation process by imagining a 'what if' scenario, such as what would happen if a writer is kidnapped by a sadistic nurse in Colorado.[111] \
+    King often uses authors as characters, or includes mention of fictional books in his stories, novellas and novels, such as Paul Sheldon, who is the main character in Misery, adult Bill Denbrough in It, Ben Mears in 'Salem's Lot, and Jack Torrance in The Shining. He has extended this to breaking the fourth wall by including himself as a character in The Dark Tower series from The Dark Tower V: Wolves of the Calla onwards. In September 2009 it was announced he would serve as a writer for Fangoria.[112]" + prompt,
     temperature: 0.9,
     max_tokens: 600,
     top_p: 0.3,
     frequency_penalty: 0.1,
     presence_penalty: 0.2,
+    n: 1,
     stop:["!!!"]
 
   });
   
-  console.log(answer.data.choices[0])
-       await setDoc(doc(db, topic, uuid), {
+  console.log(answer.data.choices)
+  await setDoc(doc(db, topic, uuid), {
   
-        text: answer.data.choices[0].text.replace('\n','').replace('\n','') + '!!!', 
+    text: 'User: ' + req.query.question + '!!!' ,
+    timestamp: new Date(), 
+    sender: uuid
+  });   
+  await setDoc(doc(db, topic, uuid), {
+  
+        text: 'You: ' + answer.data.choices[0].text.replace('\n','').replace('\n','') + '###', 
         timestamp: new Date(), 
         sender: uuid
       });
