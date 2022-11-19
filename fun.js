9b972d406fce3cac32bf2a3956175c2363bb3c34
Jarett Dunn <jarettdunn+git@gmail.com>
2022-11-19 02:36:53 -0400

    migration

--- fun.js
+++ fun.js
@@ -212,7 +212,7 @@ else {
     prompt = "this is a chatbot that rephrases the original input to sound much more genuinely informed about " + topic + " keeping the theme, tone, and intention of the original input intact, while never including adult or risquee content.\n\n"
 }
 for (var p of theprompts[uuid]){
-
+let tprompts = []
   p = p.replace(uuid, 'You')
   tprompts.push(p)
 }
