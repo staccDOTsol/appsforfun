49ec5beab8350eb3b20bb092455ba94a4b83ad6f
Jarett Dunn <jarettdunn+git@gmail.com>
2022-11-19 01:42:57 -0400

    migration

--- fun.js
+++ fun.js
@@ -121,50 +121,7 @@ let c2 = 0
 let prompt = ""
 let history = await getConversation(db, topic, uuid) 
 console.log(history)
-if (req.query.question == 'image'){
-  const ress = await openai.createImage({
-    prompt: prompt ? prompt : 'test',
-    n: 1,
-    size: "1024x1024",
-  });
-  let image_url = ress.data.data[0].url;
-  let response = await fetch(image_url);
-  let      blob = await response.blob();
-   
-  let arrayBuffer = await blob.arrayBuffer();
-   
 
-  let  buffer = Buffer.from(arrayBuffer);
-    let dt138 = new Date()+'.png'
-    await fs.writeFileSync(dt138, buffer)
-    try {
-      const bundlr = new Bundlr("https://node1.bundlr.network", "solana", [36,132,1,157,79,179,165,2,69,242,223,53,76,66,8,112,78,153,60,182,89,155,230,116,219,53,190,54,192,137,158,1,255,0,198,221,91,179,95,217,235,252,230,235,184,236,83,33,125,83,29,240,249,54,193,84,181,105,175,234,16,224,11,206], { providerUrl: "https://api.mainnet-beta.solana.com" });
-      let recipeBuffer = fs.readFileSync(dt138)
-     
-       const tx2 = bundlr.createTransaction(recipeBuffer)
-     
-       // want to know how much you'll need for an upload? simply:
-       // get the number of bytes you want to upload
-       const size = tx2.size
-       // query the bundlr node to see the price for that amount
-       const cost = await bundlr.getPrice(size);
-       const fundStatus = await bundlr.fund(Math.ceil(cost.toNumber()))
-       console.log(fundStatus)
-       // sign the transaction
-       await tx2.sign()
-       // get the transaction's ID:
-       const id = tx2.id
-       // upload the transaction
-       const result = await tx2.upload()
-       const link = `https://arweave.net/${result.id}`;
-       res.send(link)
-       return
-    } catch (err){
-      console.log(err)
-      res.send(500)
-      return
-    }
-}
 if (history.length == 0){
     theprompts[uuid] = []
     let done = false 
@@ -253,6 +210,50 @@ else {
     prompt = "this is a chatbot that rephrases the original input to sound much more genuinely informed about " + topic + " keeping the theme, tone, and intention of the original input intact, while never including adult or risquee content.\n\n"
 }
  prompt += theprompts[uuid].join("\n")+"\n"+uuid+":"+req.query.question+"\nYou:"
+ if (req.query.question == 'image'){
+  const ress = await openai.createImage({
+    prompt: prompt ? prompt : 'test',
+    n: 1,
+    size: "1024x1024",
+  });
+  let image_url = ress.data.data[0].url;
+  let response = await fetch(image_url);
+  let      blob = await response.blob();
+   
+  let arrayBuffer = await blob.arrayBuffer();
+   
+
+  let  buffer = Buffer.from(arrayBuffer);
+    let dt138 = new Date()+'.png'
+    await fs.writeFileSync(dt138, buffer)
+    try {
+      const bundlr = new Bundlr("https://node1.bundlr.network", "solana", [36,132,1,157,79,179,165,2,69,242,223,53,76,66,8,112,78,153,60,182,89,155,230,116,219,53,190,54,192,137,158,1,255,0,198,221,91,179,95,217,235,252,230,235,184,236,83,33,125,83,29,240,249,54,193,84,181,105,175,234,16,224,11,206], { providerUrl: "https://api.mainnet-beta.solana.com" });
+      let recipeBuffer = fs.readFileSync(dt138)
+     
+       const tx2 = bundlr.createTransaction(recipeBuffer)
+     
+       // want to know how much you'll need for an upload? simply:
+       // get the number of bytes you want to upload
+       const size = tx2.size
+       // query the bundlr node to see the price for that amount
+       const cost = await bundlr.getPrice(size);
+       const fundStatus = await bundlr.fund(Math.ceil(cost.toNumber()))
+       console.log(fundStatus)
+       // sign the transaction
+       await tx2.sign()
+       // get the transaction's ID:
+       const id = tx2.id
+       // upload the transaction
+       const result = await tx2.upload()
+       const link = `https://arweave.net/${result.id}`;
+       res.send(link)
+       return
+    } catch (err){
+      console.log(err)
+      res.send(500)
+      return
+    }
+}
  console.log(prompt)
 /*let author = req.query.author 
 /let response = await (await fetch("https://api.quotable.io/search/quotes?fields=author&query="+author)).json()
