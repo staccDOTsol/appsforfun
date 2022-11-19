501b58404568a1dcd17a4652051ba83123b26457
Jarett Dunn <jarettdunn+git@gmail.com>
2022-11-19 01:29:54 -0400

    migration

--- fun.js
+++ fun.js
@@ -251,7 +251,7 @@ let avg = t2 / lengths.length
 }
 else {
     prompt = "this is a chatbot that rephrases the original input to sound much more genuinely informed about " + topic + " keeping the theme, tone, and intention of the original input intact, while never including adult or risquee content.\n\n"
-}ac
+}
  prompt += theprompts[uuid].join("\n")+"\n"+uuid+":"+req.query.question+"\nYou:"
  console.log(prompt)
 /*let author = req.query.author 
