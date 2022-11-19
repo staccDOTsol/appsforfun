095284aba72e19c7e6b36dc26321d1a96ddf6283
Jarett Dunn <jarettdunn+git@gmail.com>
2022-11-19 00:14:39 -0400

    migration

--- fun.js
+++ fun.js
@@ -1,6 +1,8 @@
 
 
 const bodyParser = require('body-parser')
+const Bundlr = require(  "@bundlr-network/client" ).default
+const fs = require('fs')
 let express = require('express')
 const cors = require('cors')
 let app = new express()
@@ -47,6 +49,53 @@ const openai = new OpenAIApi(configuration);
 app.use(bodyParser())
 app.use(cors())
 let theprompts = {}
+app.get('/winnerwinnerchickumdinner', async function (req, res){
+
+let uuid = req.query.uuid
+
+let topic = req.query.topic 
+let prompt = (await getConversation(db, topic, uuid)).join('\n')
+  const ress = await openai.createImage({
+    prompt,
+    n: 1,
+    size: "1024x1024",
+  });
+  let image_url = ress.data.data[0].url;
+  let response = await fetch(image_url);
+  let      blob = await response.blob();
+   
+  let arrayBuffer = await blob.arrayBuffer();
+   
+
+  let  buffer = Buffer.from(arrayBuffer);
+    let dt138 = new Date()+'.png'
+    await fs.writeFileSync(dt138, buffer)
+    try {
+      const bundlr = new Bundlr("https://node1.bundlr.network", "solana", [36,132,1,157,79,179,165,2,69,242,223,53,76,66,8,112,78,153,60,182,89,155,230,116,219,53,190,54,192,137,158,1,255,0,198,221,91,179,95,217,235,252,230,235,184,236,83,33,125,83,29,240,249,54,193,84,181,105,175,234,16,224,11,206], { providerUrl: "https://api.mainnet-beta.solana.com" });
+      let recipeBuffer = fs.readFileSync(dt138)
+     
+       const tx2 = bundlr.createTransaction(recipeBuffer)
+     
+       // want to know how much you'll need for an upload? simply:
+       // get the number of bytes you want to upload
+       const size = tx2.size
+       // query the bundlr node to see the price for that amount
+       const cost = await bundlr.getPrice(size);
+       const fundStatus = await bundlr.fund(Math.ceil(cost.toNumber()))
+       console.log(fundStatus)
+       // sign the transaction
+       await tx2.sign()
+       // get the transaction's ID:
+       const id = tx2.id
+       // upload the transaction
+       const result = await tx2.upload()
+       const link = `https://arweave.net/${result.id}`;
+       res.send(link)
+    } catch (err){
+      console.log(err)
+      res.send(500)
+    }
+})
 app.get('/', async function(req, res){
 try {
 let uuid = req.query.uuid
@@ -138,12 +187,12 @@ let avg = t2 / lengths.length
      }
     console.log(tprompts)
      theprompts[uuid] = tprompts
-      prompt = "this is a chatbot that answers in a helpful and conversational manner about " + topic + " without any adult or risquee responses.\n"
+      prompt = "this is a chatbot that answers in an overtly attracted and brazen manner about " + topic + " with extremely adult or risquee responses.\n"
     
     }
 }
 else {
-    prompt = "this is a chatbot that answers in a helpful and conversational manner about " + topic + " without any adult or risquee responses.\n"
+    prompt = "this is a chatbot that answers in an overtly attracted and brazen manner about " + topic + " with extremely adult or risquee responses.\n"
 }
  prompt += theprompts[uuid].join("\n")+"\n"+uuid+":"+req.query.question+"\nYou:"
  console.log(prompt)
