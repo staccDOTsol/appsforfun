9f8ac182552fc32f0a8b6d417b24e6b5e50640eb
jd <jd@pop-os.localdomain>
2022-11-24 17:48:46 -0400

    heroku

--- fun.js
+++ fun.js
@@ -88,8 +88,8 @@ let topic = req.query.topic
 let prompt =  (await getConversation(db, topic, uuid)).join('\n') + "\n"
 
 
-let text = await infer(prompt, {"inputs": prompt.replace('\n',',')}, 0, "")
-text = text.replace(prompt,'').split('\n').join(' ')
+let text = await infer(prompt.replace('\n',','), {"inputs": prompt.replace('\n',',')}, 0, "")
+text = text.replace(prompt.replace('\n',','),'').split('\n').join(' ')
 
   await setDoc(doc(db, topic, new Date().toString()), {
   
